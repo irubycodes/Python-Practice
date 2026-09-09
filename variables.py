ac3c93bee96a270ca4c1778169f6ed0623ebbbcc
irubycodes <reubenojochegbe53@gmail.com>
2026-09-09 16:24:43 +0100

    variables - data types, TypeError experiment

--- variables.py
+++ variables.py
@@ -1,6 +1,6 @@
 #A variable is a container holding data.
 #A variable is a named container that holds data so you can reference and reuse it later instead of repeating the data every time.
-print(name)
+
 
 name = "Reuben"
 age = 20
@@ -9,3 +9,17 @@ language = "Python"
 print(name)
 print(age)
 print(language)
+
+name = "Reuben"        # string
+age = 20               # integer
+height = 5.9           # float (decimal number)
+is_engineer = False    # boolean (True or False)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_engineer))
+
+age = 20
+next_birthday = age + 1
+print(next_birthday)
