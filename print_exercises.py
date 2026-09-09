e40c0dc17db91b3a59cbad504a78564b93af6450
irubycodes <reubenojochegbe53@gmail.com>
2026-09-09 14:40:57 +0100

    lesson 1 - print exercises and debugging

---
+++ print_exercises.py
@@ -0,0 +1,7 @@
+print("I am starting again. This time i finish.")
+print("Reuben Israel Ojochegbe")
+print("I want to build real life projects that solve real life problems")
+print("I am starting again. This time I finish.")
+print("My name is Reuben")
+
+      
