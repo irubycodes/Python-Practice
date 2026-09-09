8af8d2b0bfc0c5eb29b6062283a74a3042dbb397
irubycodes <reubenojochegbe53@gmail.com>
2026-09-09 15:40:01 +0100

    variables - naming, strings, NameError experiment

---
+++ variables.py
@@ -0,0 +1,11 @@
+#A variable is a container holding data.
+#A variable is a named container that holds data so you can reference and reuse it later instead of repeating the data every time.
+print(name)
+
+name = "Reuben"
+age = 20
+language = "Python"
+
+print(name)
+print(age)
+print(language)
