5e97795320d9ccef82a99f55ee9224885a482fdc
irubycodes <reubenojochegbe53@gmail.com>
2026-09-15 13:38:40 +0100

    grade checker - added input validation for invalid scores

---
+++ grade_checker.py
@@ -0,0 +1,16 @@
+# Grade Checker
+
+grade = 75
+
+if grade < 0 or grade > 100:
+    print("Invalid score")
+elif grade >= 70:
+    print("A")
+elif grade >= 60:
+    print("B")
+elif grade >= 50:
+    print("C")
+elif grade >= 40:
+    print("D")
+else:
+    print("F")
